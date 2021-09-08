6ed81cb2b48df8be1ae414eb1b62ab5ff72d4ed2
riyatyagi2021 <riya.mobcoder@gmail.com>
2021-09-08 10:33:43 +0530

    dashboard

---
+++ app/src/main/java/com/example/instagram/ui/DashboardActivity.kt
@@ -0,0 +1,15 @@
+package com.example.instagram.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.instagram.R
+import com.example.instagram.base.BaseActivity
+
+class DashboardActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
